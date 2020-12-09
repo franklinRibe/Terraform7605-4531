0e6c9563471724c775b227478434338caa74cf3b
Franklin Ribeiro <franklin.ribeiro@4linux.com.br>
2020-12-08 22:36:38 -0300

    adicionando os outputs

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "vpc_id"{
+    value = google_compute_network.vpc-terraform.id
+    description = "id da vpc"     
+}
+
+output "vpc_mtu"{
+    value = google_compute_network.vpc-terraform.mtu
+    description = "id da vpc"     
+}
+
+output "vpc_gateway"{
+    value = google_compute_network.vpc-terraform.gateway_ipv4
+    description = "gateway da vpc"     
+}
